9559cbede231fc475e974ad1e3236986ad432e14
asela3 <aselajayathilake3@gmail.com>
2024-02-25 19:34:14 +0530

    Added bcrypt password hash and compare

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,6 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, BeforeInsert, BeforeUpdate, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { IsEmail } from "class-validator";
+import bcrypt from 'bcrypt';
 
 @Entity()
 export class User extends BaseEntity {
@@ -53,13 +54,30 @@ export class User extends BaseEntity {
     @Column({ type: "double precision", default: 0 })
     lastOrientation: number
 
-    get fullname(): string {
-        return `${this.firstName} ${this.lastName}`
-    }
-
     @CreateDateColumn()
     createdAt: string
 
     @UpdateDateColumn()
     updatedAt: string
+
+    get fullname(): string {
+        return `${this.firstName} ${this.lastName}`
+    }
+
+    private hashPassword = (password: string): Promise<string> => {
+        return bcrypt.hash(password, 10)
+    }
+
+    public comparePassword = (password: string): Promise<boolean> => {
+        return bcrypt.compare(password, this.password)
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    async savePassword(): Promise<void> {
+        if (this.password) {
+            const hashedPassword = await this.hashPassword(this.password);
+            this.password = hashedPassword;
+        }
+    }
 }
